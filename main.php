28b4f8a84c460e796aa5eb4beaaafd960ecd201e
amrithds <amrith.ds.gowda@gmail.com>
2018-12-04 22:42:13 +0530

    Error handling
    Error handling of json format

--- main.php
+++ main.php
@@ -3,26 +3,31 @@ require_once('json.php');
 
 $decoded_json = json_decode(json_decode($people), true);
 
+//if error then reformat json by removing extra comma(,)
+//As of now only extra comma error is handled. Can extend the format handling to specific cases
 if(json_last_error() != 0){
     $people = preg_replace("/,(?!.*,)/", "", $people);
     $decoded_json = json_decode($people, true);
 }
+if(json_last_error() == 0){
+    $concatinated_emails = '';
+    $temp_input = [];
+    if(isset($decoded_json['data'])){
+        foreach($decoded_json['data'] as $data){
+            $temp_input[$data['age']] = $data;
 
-$concatinated_emails = '';
-$temp_input = [];
-if(isset($decoded_json['data'])){
-    foreach($decoded_json['data'] as $data){
-        $temp_input[$data['age']] = $data;
-
-        $concatinated_emails .= $data['email'];
-        if (next($decoded_json['data'] )) {
-            $concatinated_emails .= ',';
+            $concatinated_emails .= $data['email'];
+            if (next($decoded_json['data'] )) {
+                $concatinated_emails .= ',';
+            }
         }
     }
-}
 
-//variable 1
-echo $concatinated_emails;
-krsort($temp_input);
-//variable 2
-$sorted_input = json_encode(['data' => array_values($temp_input)]);
+    //variable 1
+    echo $concatinated_emails;
+    krsort($temp_input);
+    //variable 2
+    $sorted_input = json_encode(['data' => array_values($temp_input)]);
+}else{
+    echo "error in json format";   
+}
